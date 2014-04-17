a6f7fbfad34ffbbc2b52cc455985532cf0fa0d5b
Andrei Zharau <andrei.zharau@here.com>
2014-04-16 21:01:46 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+NodeJS client for Samsung SmartTV
+=======
+
+Module for integration of Samsung SmartTV with your NodeJS application
+
+Usage
+-----
+
+```javascript
+var SamsungRemote = require('samsung-remote');
+var remote = new SamsungRemote({
+});
+```
---
+++ index.js
@@ -0,0 +1,11 @@
+/*!
+ *  samsung-remote
+ *  Remote client for Samsung SmartTV
+ *
+ *  @author Andrei Zharov
+ *  @license Released under the MIT license
+ *
+ *  https://github.com/natalan/samsung-remote
+ */
+
+module.exports = require('./lib/samsung-remote.js');
